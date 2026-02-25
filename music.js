4fbb9863481d3236f9f27c53bb82688505736d0e
Preethi Leelavathy <preethi.leelavathy@gmail.com>
2026-02-25 13:10:21 +0530

    content-update
    Updated some content, grouped images into a folder for a cleaner repo. Added JS to update the footer year automatically.

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Load the header content
   fetch("header.html")
     .then(response => response.text())
@@ -8,18 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
       adjustLayout();
       // Initialize the header scripts after the header is loaded
       initializeHeaderScripts();  // This function is in your main JS file
-  })
-  .catch(error => console.log('Error loading header:', error));
+    })
+    .catch(error => console.log('Error loading header:', error));
 
   console.log("DOM fully loaded and parsed");
 
   // Video controls for pausing other videos and resetting on pause or end
   const videos = document.querySelectorAll('video');
 
-  videos.forEach(function(video) {
+  videos.forEach(function (video) {
     // Pause all other videos when one is played
-    video.addEventListener('play', function() {
-      videos.forEach(function(otherVideo) {
+    video.addEventListener('play', function () {
+      videos.forEach(function (otherVideo) {
         if (otherVideo !== video && !otherVideo.paused) {
           otherVideo.pause();
           otherVideo.currentTime = 0; // Reset other video to start
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // Reset video to start and show poster when paused
-    video.addEventListener('pause', function() {
+    video.addEventListener('pause', function () {
       if (!video.ended) {
         video.currentTime = 0; // Reset to start
         video.load();
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     // When video ends, reset to start
-    video.addEventListener('ended', function() {
+    video.addEventListener('ended', function () {
       video.currentTime = 0; // Reset to start
       video.load();
     });
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Slider functionality
   const _C = document.querySelector('.container'),
-        N = _C.children.length;
+    N = _C.children.length;
 
   _C.style.setProperty('--n', N);
   _C.addEventListener('touchmove', e => { e.preventDefault() }, false);
@@ -114,134 +114,139 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener('resize', adjustLayout);
 
 
+  const yearSpan = document.querySelector("#year");
+  const currentYear = new Date().getFullYear();
+
+  yearSpan.textContent = currentYear;
+
 }); //End of DOM
 
 function initializeHeaderScripts() {
   const modal = document.getElementById("contactForm");
- const btn = document.querySelector(".fa-envelope");
- const span = document.querySelector(".close-btn");
- const form = document.getElementById("contact-form");
- const thankYouMessage = document.getElementById("thank-you-message");
+  const btn = document.querySelector(".fa-envelope");
+  const span = document.querySelector(".close-btn");
+  const form = document.getElementById("contact-form");
+  const thankYouMessage = document.getElementById("thank-you-message");
 
- if (btn) {
-     btn.onclick = function (event) {
-         event.preventDefault();
-         modal.style.display = "block";
-         console.log("Email button clicked - modal opened.");
-     };
- } else {
-     console.error("Email button not found.");
- }
-
- if (span) {
-     span.onclick = function () {
-         modal.style.display = "none";
-         console.log("Close button clicked - modal closed.");
-     };
- } else {
-     console.error("Close button not found.");
- }
-
- window.onclick = function (event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-         console.log("Click outside modal detected - modal closed.");
-     }
- };
-
- // Form submission with reCAPTCHA
- if (form) {
-     form.addEventListener("submit", function (event) {
-         event.preventDefault(); // Prevent page refresh
-
-         const submitButton = form.querySelector('button.g-recaptcha');
-         if (submitButton) {
-             submitButton.classList.add('loading'); // Apply loading state
-             submitButton.disabled = true; // Disable the button
-         }
-
-         console.log("Form submission initiated...");
-
-         const formData = new FormData(form);
-         for (let [key, value] of formData.entries()) {
-             console.log(`FormData: ${key} = ${value}`);
-         }
-
-         fetch(form.action, {
-             method: "POST",
-             body: formData,
-         })
-             .then(response => {
-                 console.log("Response received from the server.");
-                 if (response.ok) {
-                     console.log("Form submission successful, displaying thank-you message.");
-                     return response.text(); // Get response text
-                 } else {
-                     console.error("Form submission failed with status:", response.status);
-                     throw new Error("Form submission failed with status: " + response.status);
-                 }
-             })
-             .then(text => {
-                 console.log("Server response text:", text);
-                 form.style.display = "none";
-                 thankYouMessage.style.display = "block";
-
-                 setTimeout(function () {
-                     modal.style.display = "none";
-                     form.reset();
-                     form.style.display = "block";
-                     thankYouMessage.style.display = "none";
-
-                     if (submitButton) {
-                         submitButton.classList.remove('loading'); // Remove loading state
-                         submitButton.disabled = false; // Enable the button
-                     }
-
-                     console.log("Modal closed after 3 seconds.");
-                 }, 5000); // 5 seconds
-             })
-             .catch(error => {
-                 console.error("Error during form submission:", error);
-                 if (submitButton) {
-                     submitButton.classList.remove('loading'); // Remove loading state
-                     submitButton.disabled = false; // Enable the button
-                 }
-             });
-     });
- } else {
-     console.error("Contact form not found.");
- }
-
-// Side menu logic
-const menuButton = document.querySelector(".menu-button");
-const sideMenu = document.querySelector(".side-menu");
-const closeBtn = document.querySelector(".side-menu .close-btnn");
-
-if (menuButton && sideMenu) {
-  menuButton.addEventListener("click", function() {
-    console.log("Menu button clicked.");
-    sideMenu.classList.toggle("open");
-  });
-} else {
-  console.error("Menu button or side menu not found.");
-}
-
-if (closeBtn) {
-  closeBtn.addEventListener("click", function() {
-    console.log("Close button for side menu clicked.");
-    sideMenu.classList.remove("open");
-  });
-} else {
-  console.error("Close button for side menu not found.");
-}
-
-// Close the side menu when the user clicks outside of it
-window.addEventListener("click", function(event) {
-  if (sideMenu.classList.contains("open") && !sideMenu.contains(event.target) && !menuButton.contains(event.target)) {
-    console.log("Click detected outside side Menu. Closing side menu.");
-    sideMenu.classList.remove("open");
+  if (btn) {
+    btn.onclick = function (event) {
+      event.preventDefault();
+      modal.style.display = "block";
+      console.log("Email button clicked - modal opened.");
+    };
+  } else {
+    console.error("Email button not found.");
   }
-});
+
+  if (span) {
+    span.onclick = function () {
+      modal.style.display = "none";
+      console.log("Close button clicked - modal closed.");
+    };
+  } else {
+    console.error("Close button not found.");
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      console.log("Click outside modal detected - modal closed.");
+    }
+  };
+
+  // Form submission with reCAPTCHA
+  if (form) {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevent page refresh
+
+      const submitButton = form.querySelector('button.g-recaptcha');
+      if (submitButton) {
+        submitButton.classList.add('loading'); // Apply loading state
+        submitButton.disabled = true; // Disable the button
+      }
+
+      console.log("Form submission initiated...");
+
+      const formData = new FormData(form);
+      for (let [key, value] of formData.entries()) {
+        console.log(`FormData: ${key} = ${value}`);
+      }
+
+      fetch(form.action, {
+        method: "POST",
+        body: formData,
+      })
+        .then(response => {
+          console.log("Response received from the server.");
+          if (response.ok) {
+            console.log("Form submission successful, displaying thank-you message.");
+            return response.text(); // Get response text
+          } else {
+            console.error("Form submission failed with status:", response.status);
+            throw new Error("Form submission failed with status: " + response.status);
+          }
+        })
+        .then(text => {
+          console.log("Server response text:", text);
+          form.style.display = "none";
+          thankYouMessage.style.display = "block";
+
+          setTimeout(function () {
+            modal.style.display = "none";
+            form.reset();
+            form.style.display = "block";
+            thankYouMessage.style.display = "none";
+
+            if (submitButton) {
+              submitButton.classList.remove('loading'); // Remove loading state
+              submitButton.disabled = false; // Enable the button
+            }
+
+            console.log("Modal closed after 3 seconds.");
+          }, 5000); // 5 seconds
+        })
+        .catch(error => {
+          console.error("Error during form submission:", error);
+          if (submitButton) {
+            submitButton.classList.remove('loading'); // Remove loading state
+            submitButton.disabled = false; // Enable the button
+          }
+        });
+    });
+  } else {
+    console.error("Contact form not found.");
+  }
+
+  // Side menu logic
+  const menuButton = document.querySelector(".menu-button");
+  const sideMenu = document.querySelector(".side-menu");
+  const closeBtn = document.querySelector(".side-menu .close-btnn");
+
+  if (menuButton && sideMenu) {
+    menuButton.addEventListener("click", function () {
+      console.log("Menu button clicked.");
+      sideMenu.classList.toggle("open");
+    });
+  } else {
+    console.error("Menu button or side menu not found.");
+  }
+
+  if (closeBtn) {
+    closeBtn.addEventListener("click", function () {
+      console.log("Close button for side menu clicked.");
+      sideMenu.classList.remove("open");
+    });
+  } else {
+    console.error("Close button for side menu not found.");
+  }
+
+  // Close the side menu when the user clicks outside of it
+  window.addEventListener("click", function (event) {
+    if (sideMenu.classList.contains("open") && !sideMenu.contains(event.target) && !menuButton.contains(event.target)) {
+      console.log("Click detected outside side Menu. Closing side menu.");
+      sideMenu.classList.remove("open");
+    }
+  });
 
 } //end of initialise headerscripts
 
